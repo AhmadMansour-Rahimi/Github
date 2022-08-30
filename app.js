b510d33e608c83a4349ea72348f58a5f386e2369
Mansour <mansourrahimi008@gmial.com>
2022-08-30 19:24:56 +0430

    dotenv is required

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/contact' , contactRouter)
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+require('dotenv').config();
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
