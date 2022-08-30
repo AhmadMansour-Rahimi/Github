c2d069bca9ac72119dd9ca0ce047aea1d08fbf10
Mansour <mansourrahimi008@gmial.com>
2022-08-30 17:38:58 +0430

    contact route is working

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var expresslayout = require ('express-ejs-layouts')
+var expresslayout = require ('express-ejs-layouts');
+var contactRouter = require('./routes/contact')
 var app = express();
 
 // view engine setup
@@ -22,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/contact' , contactRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
